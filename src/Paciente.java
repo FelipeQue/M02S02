71def6e764822d8d8c6e519fe9f57c7068703b15
Felipe Quérette <felipe.querette@gmail.com>
2024-05-17 15:03:13 -0300

    Initial commit

---
+++ src/Paciente.java
@@ -0,0 +1,21 @@
+
+// M02S02 Exercício 01: criar classe Paciente
+
+import java.util.ArrayList;
+
+public class Paciente {
+
+    // Atributos
+    private String nome;
+    private int idade;
+    private double peso;
+    private double altura;
+    private double pressaoArterial;
+    private double frequenciaCardiaca;
+    private String dietaAlimentar;
+    private ArrayList<String> atividadesFisicas;
+
+    // Métodos
+
+
+}
