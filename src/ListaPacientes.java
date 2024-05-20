d0c780aca90ce57622ed4d3e85f8f0a0bff772d5
Felipe Quérette <felipe.querette@gmail.com>
2024-05-20 14:44:26 -0300

    Foi criada a classe ListasPacientes

---
+++ src/ListaPacientes.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+// M02S02 Exercício 02: lista de dados
+
+public class ListaPacientes {
+
+    private static ArrayList<Paciente> pacientes = new ArrayList<>();
+
+    public static void adicionarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    public static void removerPaciente(int id){
+        pacientes.remove(id);
+    }
+
+    public static void listarPacientes(){
+        System.out.println("Id - Nome");
+        for (Paciente item : pacientes){
+            System.out.println(pacientes.indexOf(item) + ". " + item.nome);
+        }
+    }
+
+    public static Paciente buscarPacienteId(int id){
+        return pacientes.get(id);
+    }
+
+
+}
